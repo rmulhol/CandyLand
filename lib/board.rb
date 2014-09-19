cf667d75c8c1d6791051d9c9a9fbe5ec4828d4c8
Robert Mulholand <robmulholand@gmail.com>
2014-09-19 16:51:15 -0500

    a day's work

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,13 @@
 class Board
+  
+  def make_100_spaces
+    new_array = []
+    100.times { new_array << [] }
+    new_array
+  end
+
+  def make_6_colors
+    colors = ["yellow", "blue", "green", "pink", "red", "orange"]
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,8 +5,25 @@ describe Board do
 
   describe "#make_100_spaces" do
     it "makes an array" do
-      expect(new_board.make_100_space.class).to eq(Array)
+      expect(new_board.make_100_spaces.class).to eq(Array)
+    end
+
+    it "makes an array with 3  subarrays" do
+      expect(new_board.make_100_spaces[(0..2)]).to eq([[], [], []])  
+    end
+
+    it "makes an array with 100 subarrays" do
+      expect(new_board.make_100_spaces.length).to eq(100)
     end
   end
+  
+  describe "#make_6_colors" do
+    it "makes an array" do
+      expect(new_board.make_6_colors.class).to eq(Array)
+    end
 
+    it "contains six elements" do
+      expect(new_board.make_6_colors.length).to eq(6)
+    end
+  end    
 end
